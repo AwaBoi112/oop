d6dd487485896751af0378387cf1133ffe52c619
AwaBoi112 <szabo.avar.csongor@student.ms.sapientia.ro>
2025-02-26 17:09:15 +0300

    .

--- labor-01/src/oop/labor01/Main.java
+++ labor-01/src/oop/labor01/Main.java
@@ -2,6 +2,6 @@ package oop.labor01;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Ujra megirom a hazit mert handi vagyok");
+        System.out.println("Ujra megirom a hazit mert handi vagyok :))))))))))))))");
     }
 }
